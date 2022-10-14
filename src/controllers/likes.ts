868596b067ca397ef727d438f0f54920ba4e21dc
bazla <dobr1nya@mail.ru>
2022-10-14 12:24:57 +0300

    refactor: fix likes controller

--- src/controllers/likes.ts
+++ src/controllers/likes.ts
@@ -4,7 +4,7 @@ import { Likes } from '../models/likes';
 export const addLike: RequestHandler = async (req, res, next) => {
   try {
     const item = await Likes.create({
-      ...req.body,
+      ...req.params,
     });
     return res.status(200).json({ message: 'Like added successfully', data: item });
   } catch (error) {
@@ -16,22 +16,22 @@ export const addLike: RequestHandler = async (req, res, next) => {
 };
 
 export const deleteLike: RequestHandler = async (req, res, next) => {
-  const { postId } = req.body;
+  const { id } = req.params;
   try {
-    const deletedLike: Likes | null = await Likes.findOne(postId);
+    const deletedLike: Likes | null = await Likes.findByPk(id);
     if (!deletedLike) {
       res.status(404).send({
-        message: `Not found Like with id ${postId}.`,
+        message: `Not found Like with id ${id}.`,
       });
     } else {
-      await Likes.destroy({ where: { postId } });
+      await Likes.destroy({ where: { id } });
       return res
         .status(200)
-        .json({ message: `Like ${postId} deleted successfully`, data: deletedLike });
+        .json({ message: `Like ${id} deleted successfully`, data: deletedLike });
     }
   } catch (error) {
     res.status(500).send({
-      message: 'Could not delete Like with id ' + postId,
+      message: 'Could not delete Like with id ' + id,
     });
   }
 };
@@ -61,39 +61,39 @@ export const getAllLikes: RequestHandler = async (req, res, next) => {
 };
 
 export const getLikeById: RequestHandler = async (req, res, next) => {
-  const { postId } = req.body;
+  const { id } = req.params;
   try {
-    const like: Likes | null = await Likes.findOne(postId);
+    const like: Likes | null = await Likes.findByPk(id);
     if (!like) {
       res.status(404).send({
-        message: `Not found Like with id ${postId}.`,
+        message: `Not found Like with id ${id}.`,
       });
     } else {
       return res.status(200).json({ message: `Like fetched successfully`, data: like });
     }
   } catch (error) {
     res.status(500).send({
-      message: 'Error retrieving Like with id ' + postId,
+      message: 'Error retrieving Like with id ' + id,
     });
   }
 };
 
 export const updateLike: RequestHandler = async (req, res, next) => {
-  const { postId } = req.body;
+  const { id } = req.params;
   try {
-    const updatedItem: Likes | null = await Likes.findOne(postId);
+    const updatedItem: Likes | null = await Likes.findByPk(id);
     if (!updatedItem) {
       res.status(404).send({
-        message: `Not found Like with id ${postId}.`,
+        message: `Not found Like with id ${id}.`,
       });
     } else {
-      await Likes.update({ ...req.body }, { where: { postId } });
-      const newLike: Likes | null = await Likes.findOne(postId);
+      await Likes.update({ ...req.params }, { where: { id } });
+      const newLike: Likes | null = await Likes.findByPk(id);
       return res.status(200).json({ message: `Item fetched successfully`, data: newLike });
     }
   } catch (error) {
     res.status(500).send({
-      message: 'Error updating Item with id ' + postId,
+      message: 'Error updating Item with id ' + id,
     });
   }
 };
